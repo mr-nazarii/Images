880ea040e95a0cb40adf09e45e8840f2d4af6395
Nazarii <nazariikubik@gmail.com>
2023-01-13 11:46:55 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+Add photos desired to compress to the raw file
+
+#1 Go to Terminal
+
+#2 Open new Terminal
+
+#3 Input the following comand
+
+```
+node run.js
+```
+
+! Compressed images in the compressed folder
---
+++ run.js
@@ -0,0 +1,40 @@
+const sharp = require("sharp");
+const fs = require("fs");
+
+// specify the folder containing the images
+const folder = "./img/raw";
+
+// specify the desired compression percentage
+const percent = 20;
+
+if (!fs.existsSync(`./img/compressed`)) {
+  fs.mkdirSync(`./img/compressed`);
+}
+
+// get a list of all files in the folder
+fs.readdir(folder, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // iterate through the files and compress each image
+  files.forEach((file) => {
+    // only process image files
+    if (!file.endsWith(".jpg") && !file.endsWith(".png")) {
+      return;
+    }
+
+    // read the image file
+    sharp(`${folder}/${file}`)
+      // compress the image
+      .jpeg({ quality: percent })
+      .toFile(`./img/compressed/${file}`)
+      .then(() => {
+        console.log(`Successfully compressed ${file}`);
+      })
+      .catch((err) => {
+        console.error(`Error compressing ${file}:`, err);
+      });
+  });
+});
